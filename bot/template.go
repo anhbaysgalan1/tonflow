d904fd4e5d4d86468ed85e2873a4fb1f67a2a9ee
Alexey Murzin <38415881+moorzeen@users.noreply.github.com>
2023-01-27 13:07:51 +0600

    inline buttons emoji removed, monospace address in confirmation message fixed

--- bot/template.go
+++ bot/template.go
@@ -1,29 +1,36 @@
 package bot
 
 const (
-	WelcomeNewUser      = "Hello, %s!\n\nYour new fast and secure wallet has been created. " + WelcomeCommon
-	WelcomeExistedUser  = "Welcome again, %s!\n\n" + WelcomeCommon
-	WelcomeCommon       = "Your wallet address is\n<code>%s</code>\n\nJust click on it to copy. The QR code of this address in the picture. You can manage your wallet using the buttons under this message.\n\n" + "Please remember that for security reasons, access to this wallet is only possible from your current Telegram account."
-	Balance             = "💎 Balance %s TON"
-	ReceiveInstruction  = "Show QR code to receive TON or use wallet address"
-	AskAmount           = "💰 How many TON to send?"
-	AskWallet           = "📲 Enter the recipient's wallet address or take a photo of QR code"
-	AskComment          = "💬 Write your comment"
-	SendingConfirmation = "✋ Confirm sending?\n\nTo wallet address: <code>%s</code>\n\nAmount: %s TON"
-	Comment             = "\n\nComment: %s"
-	NoFunds             = "🤷️ You have no TON"
-	NotEnoughFunds      = "🤷️ Not enough funds on your balance, you just have %s TON"
-	InvalidAmount       = "☝️ Only digits and one dot are allowed, try again"
-	InvalidQR           = "🤷 Unable to recognize QR code, try another photo or enter wallet address"
-	InvalidWallet       = "🤷 There is no wallet with this address"
-	Canceled            = "\n\n✅ <b>Canceled</b>"
-	ReceiveButton       = "📥 Receive"
-	SendButton          = "📤 Send"
+	WelcomeNewUser     = "Hello, %s!\n\nYour new fast and secure wallet has been created. " + WelcomeCommon
+	WelcomeExistedUser = "Welcome again, %s!\n\n" + WelcomeCommon
+	WelcomeCommon      = "Your wallet address is\n<code>%s</code>\n\nJust click on it to copy. The QR code of this address in the picture. You can manage your wallet using the buttons under this message.\n\n" + "Please remember that for security reasons, access to this wallet is only possible from your current Telegram account."
+	ReceiveInstruction = "Show QR code to receive TON or use wallet address"
+
+	Balance = "💎 Balance: %s TON"
+
+	ReceiveButton       = "Receive"
+	SendButton          = "Send"
 	BalanceButton       = "Balance"
 	UpdateBalanceButton = "Update balance"
-	AddCommentButton    = "💬 Add comment"
+	AddCommentButton    = "Add comment"
 	ConfirmButton       = "Confirm"
 	CancelButton        = "Cancel"
-	BalanceUpToDate     = "Balance is up to date"
-	BalanceUpdated      = "Balance updated"
+
+	BalanceUpToDate = "Balance is up to date"
+	BalanceUpdated  = "Balance updated"
+
+	AskAmount      = "💰 How many TON to send?"
+	NoFunds        = "🤷️ You have no TON"
+	NotEnoughFunds = "🤷️ Not enough funds on your balance, you just have %s TON"
+	InvalidAmount  = "☝️ Only digits and one dot are allowed, try again"
+
+	AskWallet     = "📲 Enter the recipient's wallet address or take a photo of QR code"
+	InvalidQR     = "🤷 Unable to recognize QR code, try another photo or enter wallet address"
+	InvalidWallet = "🤷 There is no wallet with this address"
+
+	AskComment = "💬 Ok, send your comment"
+	Comment    = "\n\nComment: %s"
+
+	SendingConfirmation = "✋ Confirm sending?\n\nTo wallet address: <code>%s</code>\n\nAmount: %s TON"
+	Canceled            = "\n\n✅ Canceled"
 )
